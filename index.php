e928ef2c1cdf74ab44544d55606db2dd74b3e33f
65160334 <boatgamer89@gmail.com>
2023-12-22 16:38:22 +0700

    Homework_3

--- index.php
+++ index.php
@@ -6,21 +6,22 @@
 <style>
 
     h1 {
-    margin-top: 0px;
-    margin-bottom: 10px;
+    
+    margin-bottom: 20px;
     font-family: sans-serif;
     font-size: 6rem;
-    background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
+    background: linear-gradient(to left, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+    text-align: center;
   }
 
-h1 {text-align: center;}
+
 p {text-align: center;}
 div {text-align: center;
 
     background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
-    
+    text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF; 
     font-size: 2rem;
     overflow-x: hidden;
     overflow-y: scroll;
@@ -28,12 +29,17 @@ div {text-align: center;
     width: 200px;
     height: 400px;
     margin: 0 auto;
-    margin-top:150px ;
-   
-  padding: 10px;
-  border: 1px solid #888;
-    
-
+    margin-top:220px ;
+    padding: 10px;
+    border: 1px solid #888;
+    animation: textclip 2s linear infinite;
+    background-image: linear-gradient(-225deg,
+                    #231557 0%,
+                    #44107a 29%,
+                    #ff1361 67%,
+                    #fff800 100%);
+                
+            
 
 
 }
@@ -45,7 +51,11 @@ div {text-align: center;
     <meta charset="utf-8">
 </head>
 <body>
-<body style="background-color:powderblue;">
+<body style="background-image: linear-gradient(-225deg,
+                    #231557 0%,
+                    #44107a 50%,
+                    #ff1361 50%,
+                    #fff800 100%);">
     <?php $multi_x = 2; 
     echo "<h1>ตารางสูตรคูณแม่ 2<?php echo $multi_x ?></h1>"?>
     

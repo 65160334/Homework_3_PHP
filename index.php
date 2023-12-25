7d1a0845704711ffc448bb2b1c4f78396d72cba1
65160334 <boatgamer89@gmail.com>
2023-12-25 17:18:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ p {text-align: center;}
 div {text-align: center;
 
     background: linear-gradient(to right, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722);
-    text-shadow: 0 0 3px #FF0000, 0 0 5px #0000FF; 
+    text-shadow: 0 0 3px #26c6da, 0 0 5px #0000FF; 
     font-size: 2rem;
     overflow-x: hidden;
     overflow-y: scroll;
